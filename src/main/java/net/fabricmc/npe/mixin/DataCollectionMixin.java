0ece3ab7c3f073d12cebda8a78e67f33406eab00
Daxanius <balder.huybreghs@gmail.com>
2022-06-27 15:27:49 +0200

    Update configs

--- src/main/java/net/fabricmc/npe/mixin/DataCollectionMixin.java
+++ src/main/java/net/fabricmc/npe/mixin/DataCollectionMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.fabricmc.npe.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.fabricmc.npe.NoPryingEyes;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class DataCollectionMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		NoPryingEyes.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
