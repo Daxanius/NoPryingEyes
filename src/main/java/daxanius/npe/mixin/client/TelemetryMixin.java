9cd363cba25587ad4d506ced7ff0ce7f13fecdb1
Daxanius <balder.huybreghs@gmail.com>
2022-07-15 13:24:59 +0200

    Simplify Telemetry Mixin

--- src/main/java/daxanius/npe/mixin/client/TelemetryMixin.java
+++ src/main/java/daxanius/npe/mixin/client/TelemetryMixin.java
@@ -8,6 +8,7 @@ import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.util.telemetry.TelemetrySender;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -18,15 +19,17 @@ import java.util.UUID;
 @Environment(EnvType.CLIENT)
 @Mixin(TelemetrySender.class)
 public class TelemetryMixin {
+	@Shadow private boolean sent;
+
 	// This does not prevent collecting telemetry data, but it prevents
 	// sending the collected telemetry data by telling
 	// it that it has already sent the data
 	@Inject(at = @At("TAIL"), method = "<init>(Lnet/minecraft/client/MinecraftClient;Lcom/mojang/authlib/minecraft/UserApiService;Ljava/util/Optional;Ljava/util/Optional;Ljava/util/UUID;)V")
 	private void init(MinecraftClient client, UserApiService userApiService, Optional<String> userId, Optional<String> clientId, UUID deviceSessionId, CallbackInfo info) {
 		NoPryingEyes.LogVerbose("Minecraft is collecting telemetry data");
-		((TelemetryAccessor) ((TelemetrySender)(Object)this)).setSent(!ConfigManager.getConfig().telemetry);
+		sent = !ConfigManager.getConfig().telemetry;
 
 		// Just to make sure nothing went wrong here
-		NoPryingEyes.LogVerbose("Blocked telemetry data: " + ((TelemetryAccessor) (TelemetrySender)(Object)this).getSent());
+		NoPryingEyes.LogVerbose("Blocked telemetry data: " + sent);
 	}
 }
