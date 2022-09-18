b809c3e07f20ec4d38c838ebd4f98c4d739e74f9
Daxanius <balder.huybreghs@gmail.com>
2022-09-18 15:59:27 +0200

    Fix stack overflow

--- src/main/java/me/daxanius/npe/mixins/client/MessageTrustStatusMixin.java
+++ src/main/java/me/daxanius/npe/mixins/client/MessageTrustStatusMixin.java
@@ -3,43 +3,71 @@ package me.daxanius.npe.mixins.client;
 import me.daxanius.npe.config.ConfigManager;
 import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.client.network.message.MessageTrustStatus;
+import net.minecraft.network.message.MessageVerifier;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 import java.time.Instant;
 
 @Mixin(MessageTrustStatus.class)
 public class MessageTrustStatusMixin {
+    // The original way of getting the status
+    private static MessageTrustStatus getStatusOriginal(SignedMessage message, Text decorated, PlayerListEntry sender, Instant receptionTimestamp) {
+        if (sender == null) {
+            return MessageTrustStatus.NOT_SECURE;
+        }
+
+        MessageVerifier.Status status = sender.getMessageVerifier().verify(message);
+        if (status == MessageVerifier.Status.BROKEN_CHAIN) {
+            return MessageTrustStatus.BROKEN_CHAIN;
+        }
+
+        if (status == MessageVerifier.Status.NOT_SECURE) {
+            return MessageTrustStatus.NOT_SECURE;
+        }
+
+        if (message.isExpiredOnClient(receptionTimestamp)) {
+            return MessageTrustStatus.NOT_SECURE;
+        }
+
+        if (!message.filterMask().isPassThrough()) {
+            return MessageTrustStatus.FILTERED;
+        }
+
+        if (message.unsignedContent().isPresent()) {
+            return MessageTrustStatus.MODIFIED;
+        }
+
+        return !decorated.contains(message.getSignedContent().decorated()) ? MessageTrustStatus.MODIFIED : MessageTrustStatus.SECURE;
+    }
 
     /**
      * @reason Removes the insecure message indicator
      * @author Daxanius
      */
 
-    @Inject(method = "getStatus(Lnet/minecraft/network/message/SignedMessage;Lnet/minecraft/text/Text;Lnet/minecraft/client/network/PlayerListEntry;Ljava/time/Instant;)Lnet/minecraft/client/network/message/MessageTrustStatus;", at = @At("HEAD"), cancellable = true)
-    private static void getStatus(SignedMessage message, Text decorated, PlayerListEntry sender, Instant receptionTimestamp, CallbackInfoReturnable<MessageTrustStatus> info) {
-        MessageTrustStatus status = MessageTrustStatus.getStatus(message, decorated, sender, receptionTimestamp);
+    @Overwrite
+    public static MessageTrustStatus getStatus(SignedMessage message, Text decorated, PlayerListEntry sender, Instant receptionTimestamp) {
+        MessageTrustStatus status = getStatusOriginal(message, decorated, sender, receptionTimestamp);
 
         switch (status) {
             case NOT_SECURE -> {
                 if (ConfigManager.getConfig().chat_indicator.hide_red) {
-                    info.setReturnValue(MessageTrustStatus.SECURE);
-                    break;
+                    return MessageTrustStatus.SECURE;
                 }
-                info.setReturnValue(status);
+                return status;
             }
             case MODIFIED, FILTERED -> {
                 if (ConfigManager.getConfig().chat_indicator.hide_yellow) {
-                    info.setReturnValue(MessageTrustStatus.SECURE);
-                    break;
+                    return MessageTrustStatus.SECURE;
                 }
-                info.setReturnValue(status);
+                return status;
             }
-            default -> info.setReturnValue(status);
+            default -> {
+                return status;
+            }
         }
     }
 }
