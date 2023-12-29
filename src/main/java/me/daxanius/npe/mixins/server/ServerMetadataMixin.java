f1064561609f5ea98e3785391c1abc61fbf0b3b8
TechPro424 <smsvra6@gmail.com>
2023-12-29 23:52:33 +0530

    Made preventsChatReports public, annotated it with @Unique Removed @SuppressWarnings annotation

--- src/main/java/me/daxanius/npe/mixins/server/ServerMetadataMixin.java
+++ src/main/java/me/daxanius/npe/mixins/server/ServerMetadataMixin.java
@@ -1,12 +1,13 @@
 package me.daxanius.npe.mixins.server;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 
 import me.daxanius.npe.config.NoPryingEyesConfig;
 import net.minecraft.server.ServerMetadata;
 
 @Mixin(ServerMetadata.class)
-public class ServerMetadataMixin {
-    @SuppressWarnings("unused")
-    private boolean preventsChatReports = NoPryingEyesConfig.getInstance().noKey();
+public abstract class ServerMetadataMixin {
+    @Unique
+    public boolean preventsChatReports = NoPryingEyesConfig.getInstance().noKey();
 }
