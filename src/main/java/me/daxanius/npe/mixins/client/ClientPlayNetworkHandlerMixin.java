3abb8694e44857b62b71c3792acd5ce84295cee5
TechPro424 <smsvra6@gmail.com>
2024-01-05 23:14:26 +0530

    Delete MessageHandlerMixin as it had no effect added injects to ClientPlayNetworkHandlerMixin instead

--- src/main/java/me/daxanius/npe/mixins/client/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/me/daxanius/npe/mixins/client/ClientPlayNetworkHandlerMixin.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.client.network.ServerInfo;
 import net.minecraft.client.toast.SystemToast;
 import net.minecraft.network.packet.s2c.play.ServerMetadataS2CPacket;
 import net.minecraft.text.Text;
@@ -13,10 +14,15 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import me.daxanius.npe.NoPryingEyes;
+import me.daxanius.npe.config.NoPryingEyesConfig;
+import me.daxanius.npe.gui.NoPryingEyesWarningScreen;
 
 @Environment(EnvType.CLIENT)
 @Mixin(ClientPlayNetworkHandler.class)
-public class ClientPlayNetworkHandlerMixin {
+public abstract class ClientPlayNetworkHandlerMixin {
     @Final
     @Shadow
     private static final Text UNSECURE_SERVER_TOAST_TITLE = Text.translatable("npe.title");
@@ -27,7 +33,22 @@ public class ClientPlayNetworkHandlerMixin {
 
     private MinecraftClient client = ((ClientCommonNetworkHandlerAccessor)this).getClient();
 
-   
+    ServerInfo serverInfo = ((ClientPlayNetworkHandler)(Object)this).getServerInfo();
+
+    
+    /**
+     * @reason Common method to intercept messages/commands from client
+     * @author TechPro424
+     */
+    
+
+    private void interceptMessages() {
+        Text warning = Text.translatable("npe.warning.on_demand");
+        NoPryingEyes.LogVerbose("Setting warn screen");
+        MinecraftClient.getInstance().setScreen(new NoPryingEyesWarningScreen(warning));
+        NoPryingEyes.LogVerbose("Enabling sign for 1 session");
+        NoPryingEyesConfig.getInstance().setTempSign(true);
+    }
 
     /**
      * @reason Add a warning message
@@ -41,4 +62,32 @@ public class ClientPlayNetworkHandlerMixin {
             this.client.getToastManager().add(systemToast);
         }
     }
+
+
+    /**
+     * @reason injects to intercept messages/commands from client and show warning scrren
+     * @author TechPro424
+     */
+    
+    @Inject(method = "sendChatMessage", at = @At("HEAD"), cancellable = true)
+    private void sendChatMessage(String content, CallbackInfo ci) {
+
+        if(serverInfo != null) if (serverInfo.isSecureChatEnforced() && NoPryingEyesConfig.getInstance().onDemand() && !NoPryingEyesConfig.getInstance().tempSign()) interceptMessages();
+        
+    }
+
+    @Inject(method = "sendChatCommand", at = @At("HEAD"), cancellable = true)
+    private void sendChatCommand(String content, CallbackInfo ci) {
+
+        if(serverInfo != null) if (serverInfo.isSecureChatEnforced() && NoPryingEyesConfig.getInstance().onDemand() && !NoPryingEyesConfig.getInstance().tempSign()) interceptMessages();
+        
+    }
+
+    @Inject(method = "sendCommand", at = @At("HEAD"), cancellable = true)
+    private void sendCommand(String content, CallbackInfoReturnable<Boolean> cir) {
+
+        if(serverInfo != null) if (serverInfo.isSecureChatEnforced() && NoPryingEyesConfig.getInstance().onDemand() && !NoPryingEyesConfig.getInstance().tempSign()) interceptMessages();
+        
+    }
+    
 }
