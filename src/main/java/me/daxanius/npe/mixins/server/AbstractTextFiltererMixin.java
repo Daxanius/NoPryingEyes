9297f8f20103b172397e53cfd66f178c70c00f54
Niterux <parkerdec@gmail.com>
2025-06-22 01:42:01 -0400

    Add configuration for profanity filter back (I forgot it)

--- src/main/java/me/daxanius/npe/mixins/server/AbstractTextFiltererMixin.java
+++ src/main/java/me/daxanius/npe/mixins/server/AbstractTextFiltererMixin.java
@@ -3,6 +3,8 @@ package me.daxanius.npe.mixins.server;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import me.daxanius.npe.NoPryingEyes;
+import me.daxanius.npe.config.NoPryingEyesConfig;
 import net.minecraft.server.filter.AbstractTextFilterer;
 import net.minecraft.server.filter.FilteredMessage;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,9 +23,12 @@ public class AbstractTextFiltererMixin {
      * so that profanity is allowed
      * @author Daxanius
      */
-
     @WrapOperation(at = @At(value = "INVOKE", target = "Ljava/util/concurrent/CompletableFuture;supplyAsync(Ljava/util/function/Supplier;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;"), method = "filter(Lcom/mojang/authlib/GameProfile;Ljava/lang/String;Lnet/minecraft/server/filter/AbstractTextFilterer$HashIgnorer;Ljava/util/concurrent/Executor;)Ljava/util/concurrent/CompletableFuture;")
-    private CompletableFuture<FilteredMessage> filterMessage(Supplier<Object> supplier, Executor executor, Operation<CompletableFuture<Object>> original, @Local(ordinal = 0, argsOnly = true) String raw) {
-        return CompletableFuture.completedFuture(FilteredMessage.permitted(raw));
+    private CompletableFuture<Object> filterMessage(Supplier<Object> supplier, Executor executor, Operation<CompletableFuture<Object>> original, @Local(ordinal = 0, argsOnly = true) String raw) {
+        if (NoPryingEyesConfig.getInstance().disable_profanity_filter) {
+            NoPryingEyes.LogVerbose("Passing message as permitted");
+            return CompletableFuture.completedFuture(FilteredMessage.permitted(raw));
+        }
+        return original.call(supplier, executor);
     }
 }
