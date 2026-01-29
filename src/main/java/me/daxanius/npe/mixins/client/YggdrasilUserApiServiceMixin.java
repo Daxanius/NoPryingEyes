fdc81cb58ca771eb2c177640425a747b851b6295
Daxanius <balder.huybreghs@gmail.com>
2026-01-29 17:28:02 +0100

    Always provide offline user properties

--- src/main/java/me/daxanius/npe/mixins/client/YggdrasilUserApiServiceMixin.java
+++ src/main/java/me/daxanius/npe/mixins/client/YggdrasilUserApiServiceMixin.java
@@ -3,8 +3,10 @@ package me.daxanius.npe.mixins.client;
 import java.util.UUID;
 import java.util.concurrent.Executor;
 
+import com.mojang.authlib.minecraft.UserApiService;
 import com.mojang.authlib.minecraft.report.AbuseReportLimits;
 import com.mojang.authlib.yggdrasil.request.AbuseReportRequest;
+import com.mojang.authlib.yggdrasil.response.KeyPairResponse;
 import me.daxanius.npe.NoPryingEyes;
 import me.daxanius.npe.config.NoPryingEyesConfig;
 import me.daxanius.npe.util.telemetry.AbuseReportLimitsCustom;
@@ -32,15 +34,22 @@ public class YggdrasilUserApiServiceMixin {
         NoPryingEyes.LogVerbose("Creating telemetry session");
     }
 
+    @Inject(method = "getKeyPair()Lcom/mojang/authlib/yggdrasil/response/KeyPairResponse;", at = @At("HEAD"), cancellable = true)
+    public void onGetKeyPair(CallbackInfoReturnable<KeyPairResponse> info) {
+        if (NoPryingEyesConfig.getInstance().noKey()) {
+            info.setReturnValue(null);
+        }
+    }
+
     @Inject(method = "isBlockedPlayer(Ljava/util/UUID;)Z", at = @At("HEAD"), cancellable = true)
-    public void isBlockedPlayer(final UUID playerID, CallbackInfoReturnable<Boolean> info) {
+    public void onIsBlockedPlayer(final UUID playerID, CallbackInfoReturnable<Boolean> info) {
         if (NoPryingEyesConfig.getInstance().disable_global_bans) {
             info.setReturnValue(false);
         }
     }
 
     @Inject(method = "reportAbuse(Lcom/mojang/authlib/yggdrasil/request/AbuseReportRequest;)V", at = @At("HEAD"), cancellable = true)
-    public void reportAbuse(final AbuseReportRequest request, CallbackInfo info) {
+    public void onReportAbuse(final AbuseReportRequest request, CallbackInfo info) {
         // Just completely ignore any attempted report requests
         info.cancel();
     }
@@ -52,7 +61,12 @@ public class YggdrasilUserApiServiceMixin {
     }
 
     @Inject(method = "getAbuseReportLimits()Lcom/mojang/authlib/minecraft/report/AbuseReportLimits;", at = @At("HEAD"), cancellable = true)
-    public void getAbuseReportLimits(CallbackInfoReturnable<AbuseReportLimits> info) {
+    public void onGetAbuseReportLimits(CallbackInfoReturnable<AbuseReportLimits> info) {
         info.setReturnValue(AbuseReportLimitsCustom.ZERO);
+    }
+
+    @Inject(method = "fetchProperties()Lcom/mojang/authlib/minecraft/UserApiService$UserProperties;", at = @At("HEAD"), cancellable = true)
+    public void onFetchProperties(CallbackInfoReturnable<UserApiService.UserProperties> info) {
+        info.setReturnValue(UserApiService.OFFLINE_PROPERTIES);
     }
 }
