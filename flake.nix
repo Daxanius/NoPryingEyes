24e7c3cc2cf72af526c50b0b84de450503554fec
Daxanius <balder.huybreghs@gmail.com>
2026-07-05 15:07:10 +0200

    Add nix flake for development
    Due to Minecraft not finding GLX (OpenGL)
    on NixOS, I added a flake with an environment variable pointing to the
    right libglvnd path. Also added .envrc for convenience of using the flake.

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Minecraft mod dev shell";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.${system}.default = pkgs.mkShell {
+        shellHook = ''
+            export LD_LIBRARY_PATH="''${LD_LIBRARY_PATH}''${LD_LIBRARY_PATH:+:}${pkgs.libglvnd}/lib"
+        '';
+      };
+    };
+}
