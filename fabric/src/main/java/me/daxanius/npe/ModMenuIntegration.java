b9278c52714bdea4b866043b5192380e95bc9bd8
Daxanius <balder.huybreghs@gmail.com>
2026-07-13 22:51:19 +0200

    Disable javadocs due to tag confusion

--- fabric/src/main/java/me/daxanius/npe/ModMenuIntegration.java
+++ fabric/src/main/java/me/daxanius/npe/ModMenuIntegration.java
@@ -8,6 +8,7 @@ import net.fabricmc.loader.api.FabricLoader;
 public class ModMenuIntegration implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        // TODO: fix broken config screen warning when cloth config is not installed
         if (!FabricLoader.getInstance().isModLoaded("cloth-config")) {
             return null;
         }
